820e7a87796631e84bacea020ddc2a433e5df627
Hanzalah Waheed <hanzalah.w@gmail.com>
2025-08-30 01:33:14 +0400

    fix: slider styling for border radius improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -319,7 +319,20 @@ export default function ABImageGenerator() {
                     max="50"
                     value={borderRadius}
                     onChange={(e) => setBorderRadius(Number(e.target.value))}
-                    className="w-full h-2 bg-neutral-200 dark:bg-neutral-700 rounded-lg appearance-none cursor-pointer slider"
+                    className="
+    w-full h-2 bg-neutral-100 dark:bg-neutral-700 rounded-lg appearance-none cursor-pointer
+    [&::-webkit-slider-thumb]:appearance-none
+    [&::-webkit-slider-thumb]:h-4
+    [&::-webkit-slider-thumb]:w-4
+    [&::-webkit-slider-thumb]:rounded-full
+    [&::-webkit-slider-thumb]:bg-neutral-500
+    [&::-webkit-slider-thumb]:cursor-pointer
+    [&::-moz-range-thumb]:h-4
+    [&::-moz-range-thumb]:w-4
+    [&::-moz-range-thumb]:rounded-full
+    [&::-moz-range-thumb]:bg-neutral-500
+    [&::-moz-range-thumb]:cursor-pointer
+  "
                   />
                 </div>
               </div>
